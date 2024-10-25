5cb02b0f4a5d61c152164522089f9cca84c6aa98
Raulro03 <raulromeraortiz@gmail.com>
2024-10-25 16:05:26 +0200

    Paso 4 final diferentes maneras de ver la descripcion en distintas rutas

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,10 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->when($request->is('api/categories*'), function () {
+            'description' => $this->when($request->is('api/categories*'), function () use ($request) {
+                if($request->is('api/categories')){
+                    return str($this->description)->limit(20);
+                }
                 return $this->description;
             }),
         ];
