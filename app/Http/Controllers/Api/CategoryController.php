7aaff40c135c256e940b926641f2c93f93f461f6
Raulro03 <raulromeraortiz@gmail.com>
2024-10-29 16:42:40 +0100

    Paso 7 a la hora de crear una categoria hemos hecho que si no le pasamos el name muestre un error personalizado

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -16,7 +17,7 @@ class CategoryController extends Controller
         return new CategoryResource($category);
     }
 
-    public function store(Request $request){
+    public function store(StoreCategoryRequest $request){
         $category = Category::create($request->all());
 
         return new CategoryResource($category);
