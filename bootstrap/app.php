8ae8cde0d425cf3e2689aca9d282af79dcc619a0
Raulro03 <raulromeraortiz@gmail.com>
2024-11-09 11:47:19 +0100

    FIn paso 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi(); // se asegura de pedir autenticacion en cada peticion
         $middleware->prependToGroup('api', AllwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
